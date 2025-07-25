1d1181d5fbd16c4a90ea8a3a1d3f2cbd16fed16c
CurtisHai <119610670+CurtisHai@users.noreply.github.com>
2025-07-25 12:42:36 +0100

    Add admin interfaces for workflows and runs

--- equipment_booking_app/workflow_automation/admin.py
+++ equipment_booking_app/workflow_automation/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import Equipment, Booking, Profile, LoginAttempt, UnknownLoginAttempt, Workflow, WorkflowStep
+from .models import (
+    Equipment,
+    Booking,
+    Profile,
+    LoginAttempt,
+    UnknownLoginAttempt,
+    Workflow,
+    WorkflowStep,
+    WorkflowRun,
+)
 
 # Admin interface for Equipment model
 class EquipmentAdmin(admin.ModelAdmin):
@@ -24,6 +33,37 @@ class ProfileInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
 
+
+class WorkflowAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "created_by",
+        "created_at",
+        "is_published",
+        "from_shared",
+    )
+    list_filter = ("is_published", "from_shared", "created_at")
+    search_fields = ("name", "description", "created_by__username")
+
+
+class WorkflowStepAdmin(admin.ModelAdmin):
+    list_display = ("workflow", "step_type", "order")
+    list_filter = ("workflow", "step_type")
+    search_fields = ("workflow__name",)
+
+
+class WorkflowRunAdmin(admin.ModelAdmin):
+    list_display = (
+        "workflow",
+        "user",
+        "started_at",
+        "completed_at",
+        "initials",
+        "project_code",
+    )
+    list_filter = ("workflow", "user", "started_at", "completed_at")
+    search_fields = ("workflow__name", "user__username", "project_code", "initials")
+
 # Unregister the default User admin and register the custom one
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
@@ -32,5 +72,6 @@ admin.site.register(Booking, BookingAdmin)
 admin.site.register(Profile)
 admin.site.register(LoginAttempt)
 admin.site.register(UnknownLoginAttempt)
-admin.site.register(Workflow)
-admin.site.register(WorkflowStep)
+admin.site.register(Workflow, WorkflowAdmin)
+admin.site.register(WorkflowStep, WorkflowStepAdmin)
+admin.site.register(WorkflowRun, WorkflowRunAdmin)
